484b79d5294eb450ac049c48bcbd4d702250c108
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-16 22:40:18 -0400

    fix: refactor generateRelease to use async/await for improved asynchronous handling

--- src/module/release/generate_release.ts
+++ src/module/release/generate_release.ts
@@ -8,14 +8,14 @@ import { logger } from '@src/util/logger.js';
 
 import type { CommitType } from '@src/type/commit_type.js';
 
-const generateRelease = (nextVersion: string, releaseNotes: string, commits: CommitType[]) => {
+const generateRelease = async (nextVersion: string, releaseNotes: string, commits: CommitType[]) => {
   updatePackageJson(nextVersion);
-  changelog(commits);
-  commitStagedVersionFiles(nextVersion);
-  createTag(nextVersion);
-  gitPushHead();
-  gitPushTag(nextVersion);
-  githubCreateRelease(nextVersion, releaseNotes);
+  await changelog(commits);
+  await commitStagedVersionFiles(nextVersion);
+  await createTag(nextVersion);
+  await gitPushHead();
+  await gitPushTag(nextVersion);
+  await githubCreateRelease(nextVersion, releaseNotes);
   logger.success(`Release process completed successfully! New version: 🚀 ${nextVersion}`);
 };
 
