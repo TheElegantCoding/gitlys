49b2bfa6d1f20a9331992c219f96029933902c1a
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-16 22:05:54 -0400

    fix: refactor commitStagedVersionFiles and getStagedCommit to use async/await for improved asynchronous handling

--- src/module/git/git_commit.ts
+++ src/module/git/git_commit.ts
@@ -1,25 +1,24 @@
 import { tagExists } from '@src/module/git/git_tag.js';
+import { execAsync } from '@src/util/command_runner.js';
 import { getConfiguration } from '@src/util/file_configuration.js';
 import { reportLogger } from '@src/util/file_logger.js';
 import { logger, loggerLoader } from '@src/util/logger.js';
 import { colorAnsi, loggerStyle } from 'logginlys';
-import { execSync } from 'node:child_process';
 
 import type { CommitType } from '@src/type/commit_type.js';
 
-const commitStagedVersionFiles = (nextVersion: string): void => {
+const commitStagedVersionFiles = async (nextVersion: string) => {
   const loader = loggerLoader(`Committing staged files for version ${nextVersion}...`);
   const config = getConfiguration();
   const changelogPath = config.changelog.changelogPath ?? 'CHANGELOG.md';
   loader.start();
 
-  if (config.changelog.generateChangelog) {
-    execSync(`git add package.json ${changelogPath}`, { stdio: 'pipe' });
-  } else {
-    execSync('git add package.json', { stdio: 'pipe' });
-  }
+  await (config.changelog.generateChangelog
+    ? execAsync(`git add package.json ${changelogPath}`)
+    : execAsync('git add package.json'));
 
-  execSync(`git commit -m "release: ${nextVersion}"`, { stdio: 'pipe' });
+  await execAsync(`git commit -m "release: ${nextVersion}"`);
+
   loader.stop();
 };
 
@@ -35,10 +34,10 @@ const checkStagedCommits = (commitStaged: CommitType[]) => {
   return filteredResult;
 };
 
-const getStagedCommit = (currentVersion: string, reportCommits = true) => {
-  const commits = tagExists()
-    ? execSync(`git log v${currentVersion}..HEAD --pretty=format:"%s|%h|%an"`).toString().trim().split('\n')
-    : execSync('git log --pretty=format:"%s|%h|%an"').toString().trim().split('\n');
+const getStagedCommit = async (currentVersion: string, reportCommits = true) => {
+  const commits = await tagExists()
+    ? (await execAsync(`git log v${currentVersion}..HEAD --pretty=format:"%s|%h|%an"`)).toString().trim().split('\n')
+    : (await execAsync('git log --pretty=format:"%s|%h|%an"')).toString().trim().split('\n');
 
   const result: CommitType[] = [];
 
