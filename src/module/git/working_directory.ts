0f6e08d0acb4dfa504933c8ad097338f49d2b4b1
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-16 22:50:03 -0400

    fix: refactor isWorkingDirectoryClean for improved readability

--- src/module/git/working_directory.ts
+++ src/module/git/working_directory.ts
@@ -1,7 +1,8 @@
 import { execAsync } from '@src/util/command_runner.js';
 
 const isWorkingDirectoryClean = async () => {
-  const status = (await execAsync('git status --porcelain')).toString().trim();
+  const { stdout } = await execAsync('git status --porcelain');
+  const status = stdout.toString().trim();
 
   if (status.length > 0) {
     throw new Error('Working directory is not clean. Please commit or stash your changes before releasing.');
